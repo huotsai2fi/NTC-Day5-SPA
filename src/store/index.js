5a317e2d58d80f123b6851b00b9fe3b83cd238a0
April Hung <huotsai2fi@gmail.com>
2022-03-06 14:28:54 +0800

    fix: newProduct.product undefined bug

--- src/store/index.js
+++ src/store/index.js
@@ -1,23 +1,25 @@
-import { createStore } from "vuex";
+import { createStore } from 'vuex';
 import createPersistedState from 'vuex-persistedstate';
 
 export default createStore({
   state: {
-    serverPath: 'http://35.234.34.149',
+    serverPath: 'https://158.101.150.132',
     cart: [],
   },
   getters: {
     currentQuantity(state) {
-      return state.cart.reduce((acc,cur) => acc += cur.number,0);
-    }
+      return state.cart.reduce((acc, cur) => (acc += cur.number), 0);
+    },
   },
   mutations: {
     addCart(state, data) {
-      const product = state.cart.find((product) => product.id === data.product.id );
+      const product = state.cart.find(
+        (product) => product.id === data.product.id
+      );
       if (product) product.number += data.number;
       else {
         const newProduct = data.product;
-        newProduct.product.number = data.number;
+        newProduct.number = data.number;
         state.cart.push(newProduct);
       }
       // console.log(state.cart);
@@ -25,7 +27,5 @@ export default createStore({
   },
   actions: {},
   modules: {},
-  plugins: [
-    createPersistedState(),
-  ]
+  plugins: [createPersistedState()],
 });
